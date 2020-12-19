52f9f041a216feb4a6863871aae2f7c5ef9be2db
Alexi Krol <krol.mats@gmail.com>
2020-12-19 18:12:12 +0200

    change recurcion method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,34 @@
 package com.company;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
-        int result = sumDigits(669);
+        System.out.println("Введите любое число: ");
+
+        int result = s.nextInt();
+        int summa = sumDigits(result);
+
+        System.out.println("Сумма чисел которые вы вели: " + sumDigits(result));
     }
+
 
     public static int sumDigits(int number) {
-
-        int sum = 0;
-        if (number >= 10) {
-            for (int i = number; number != 0; number /= 10) {
-                sum += number % 10;
-
-            }
-            System.out.println("Сумма чисел: " + sum);
-
+        if (number / 10 >= 1) {
+            int oneVar = number % 10;
+            int remain = number / 10;
+            return oneVar + sumDigits(remain);
+        } else {
+            return number;
         }
-        return -1;
+
     }
 }
+
 
 
 
